6c9fa29f598f3dee87d9fd04835f17f45f3802c4
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-05-05 13:20:31 +0600

    Add logger to telegram

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 import asyncio
+import logging
 import requests
 import telegram
 import time
 
 from environs import Env
-from pprint import pprint
 
 
-async def main():
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(self.chat_id, text=log_entry)
+
+
+def send_request_to_dvmn(dvmn_token, timestamp):
     url = 'https://dvmn.org/api/long_polling'
+    headers = {'Authorization': f'Token {dvmn_token}'}
+    params = {'timestamp': timestamp}
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
 
     env = Env()
     env.read_env()
 
+    timestamp = ''
+    dvmn_token = env('DVMN_TOKEN')
+
     telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
     telegram_bot = telegram.Bot(token=telegram_bot_token)
-
     telegram_channel_id = env('TELEGRAM_CHANNEL')
 
-    dvmn_token = env('DVMN_TOKEN')
-    headers = {'Authorization': f'Token {dvmn_token}'}
-    timestamp = ''
-    params = {'timestamp': timestamp}
+    logging.basicConfig(format="%(process)d %(levelname)s %(message)s")
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(telegram_bot, telegram_channel_id))
 
+    logger.info('Bot is started...')
     while True:
         try:
-            response = requests.get(url, params=params, headers=headers)
-            response.raise_for_status()
-            task_status_content = response.json()
+            task_status_content = send_request_to_dvmn(dvmn_token, timestamp)
             if task_status_content['status'] == 'timeout':
                 timestamp = task_status_content['timestamp_to_request']
             if not task_status_content['status'] == 'timeout':
@@ -40,17 +61,17 @@ async def main():
                 if task_status_content['new_attempts'][0]['is_negative']:
                     text = f'У вас проверили работу <<{lesson_title}>>\n'\
                            f'К сожалению, в работе есть ошибки.'
-                async with telegram_bot:
-                    await telegram_bot.send_message(
-                        chat_id=telegram_channel_id, 
-                        text=text
-                    )
+                telegram_bot.send_message(
+                    chat_id=telegram_channel_id, 
+                    text=text
+                )
         except requests.exceptions.ReadTimeout as error:
             pass
         except requests.exceptions.ConnectionError as error:
-            print(error)
+            logger.error(error)
             time.sleep(180)
 
 
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
